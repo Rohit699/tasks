37a6e5018050fb6a9afc345ccd7171ff91ed3b84
Rohit Gupta <52736642+Rohit699@users.noreply.github.com>
2025-05-15 23:52:17 +0530

    Upload the task 1 and 2

---
+++ task1.py
@@ -0,0 +1,11 @@
+# mathematical operations
+
+a=input("Enter first value: ")
+a=int(a)
+b=input("Enter second value: ")
+b=int(b)
+sum=a+b
+sub=a-b
+div=a/b
+mul=a*b
+print("Addition: ",sum,"\nSubtraction: ",sub,"\nMultiplication: ",mul,"\nDivision: ",div)
---
+++ task2.py
@@ -0,0 +1,5 @@
+# Personalised Greeting
+
+a=str(input("Enter first name: "))
+b=input("Enter second name: ")
+print("\n\nHello, ",a +" ",b+"! Welcome to the python program")
